fa63ff2afa79d8c3067139f049f93ae54c062b3d
Skovgaard <Sk0vgaard@live.dk>
2017-08-17 10:24:17 +0200

    Startup

--- VideoMenu/Program.cs
+++ VideoMenu/Program.cs
@@ -6,7 +6,8 @@ namespace VideoMenu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+          Console.ReadLine();
         }
     }
 }
